d983c8b5d800a416749ceff2cbe9aa3fce4507b6
Giiifdz <angelgibranix@gmail.com>
2026-09-14 14:59:20 -0600

    Configuracion de rutas del sistema con Auth Guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,33 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login';
-import { DashboardComponent } from './pages/dashboard/dashboard';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'clientes',
+    loadComponent: () => import('./pages/clientes/clientes.component').then(m => m.ClientesComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'reservaciones',
+    loadComponent: () => import('./pages/reservaciones/reservaciones.component').then(m => m.ReservacionesComponent),
+    canActivate: [authGuard]
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
 ];
